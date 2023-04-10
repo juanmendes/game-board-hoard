7ba7239a72c710a1f651a5762eedb3efa4ffa267
Juan Mendes <mendejuan@gmail.com>
2023-04-10 15:20:20 +0300

    Lab 10 - Manual work for creating header storybook story

--- libs/store/ui-shared/src/lib/header/header.component.stories.ts
+++ libs/store/ui-shared/src/lib/header/header.component.stories.ts
@@ -1,9 +1,16 @@
-import { Meta } from '@storybook/angular';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header.component';
+import { Meta, moduleMetadata } from '@storybook/angular';
 
 export default {
   title: 'HeaderComponent',
   component: HeaderComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatToolbarModule],
+    }),
+    //...
+  ],
 } as Meta<HeaderComponent>;
 
 export const Primary = {
