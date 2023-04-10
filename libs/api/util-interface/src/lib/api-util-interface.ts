18e7edf88c90aa319966a862292e99eaeb252bd4
Juan Mendes <mendejuan@gmail.com>
2023-04-10 14:29:06 +0300

    Lab 9 - Make backend use DTOs

--- libs/api/util-interface/src/lib/api-util-interface.ts
+++ libs/api/util-interface/src/lib/api-util-interface.ts
@@ -1,3 +1,8 @@
-export function apiUtilInterface(): string {
-  return 'api-util-interface';
+export interface Game {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  rating: number;
 }
