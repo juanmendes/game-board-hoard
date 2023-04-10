92138f2e2be0197df38d66dc45686dfff322b372
Juan Mendes <mendejuan@gmail.com>
2023-04-10 13:23:32 +0300

    Lab 5 - formatRating utility function

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-    import { getAllGames } from '../fake-api';
+import {Component} from '@angular/core';
+import {getAllGames} from '../fake-api';
+import {formatRating} from "@bg-hoard/store/util-formatters";
 
-    @Component({
-      selector: 'bg-hoard-root',
-      templateUrl: './app.component.html',
-      styleUrls: ['./app.component.css']
-    })
-    export class AppComponent {
-      title = 'Board Game Hoard';
-      games = getAllGames();
-    }
+@Component({
+  selector: 'bg-hoard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Board Game Hoard';
+  games = getAllGames();
+  formatRating = formatRating
+}
--- libs/store/util-formatters/src/lib/store-util-formatters.ts
+++ libs/store/util-formatters/src/lib/store-util-formatters.ts
@@ -1,3 +1,4 @@
-export function storeUtilFormatters(): string {
-    return 'store-util-formatters';
+export function formatRating(rating: number) {
+    return `${Math.round(rating * 100) / 10} / 10`;
+
 }
