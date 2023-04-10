12678989d428a991ab12a7560e48bfa7bcb9f355
Juan Mendes <mendejuan@gmail.com>
2023-04-10 15:25:51 +0300

    Lab 11 - Manual work to e2e test header title

--- apps/store-ui-shared-e2e/src/e2e/header/header.component.cy.ts
+++ apps/store-ui-shared-e2e/src/e2e/header/header.component.cy.ts
@@ -1,8 +1,10 @@
 describe('store-ui-shared', () => {
   beforeEach(() =>
-    cy.visit('/iframe.html?id=headercomponent--primary&args=title;')
+    cy.visit(
+      '/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard;'
+    )
   );
   it('should render the component', () => {
-    cy.get('bg-hoard-header').should('exist');
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });
