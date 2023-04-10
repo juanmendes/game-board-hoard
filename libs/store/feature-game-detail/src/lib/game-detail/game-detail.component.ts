409955b5792d736c2b32217391ca81b1ddffe0f8
Juan Mendes <mendejuan@gmail.com>
2023-04-10 13:35:22 +0300

    Lab 5 - Generate game detail component
    Command
    
    nx generate @nrwl/angular:component game-detail  --project=store-feature-game-detail

---
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bg-hoard-game-detail',
+  templateUrl: './game-detail.component.html',
+  styleUrls: ['./game-detail.component.css']
+})
+export class GameDetailComponent {
+
+}
--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -3,12 +3,16 @@
         import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { storeFeatureGameDetailRoutes } from './lib.routes';
+import { GameDetailComponent } from './game-detail/game-detail.component';
         
         @NgModule({
           imports: [
             CommonModule,
             
     RouterModule.forChild(storeFeatureGameDetailRoutes) 
+          ],
+          declarations: [
+            GameDetailComponent
           ]
         })
         export class StoreFeatureGameDetailModule { }
